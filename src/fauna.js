721c4bb5120b92c7b48cb5a836fdabe31c3656e2
Kathrin92 <kathrinulmer@gmx.de>
2020-02-28 16:13:07 +0100

    Add two buttons and change header

--- src/fauna.js
+++ src/fauna.js
@@ -3,6 +3,7 @@ function kiwi() {
   container.className = "card";
   const heading = document.createElement("h2");
   heading.className = "cardheader";
+  heading.id = "newheading";
   const image = document.createElement("img");
   image.className = "cardimage";
   const paragraph = document.createElement("p");
@@ -22,3 +23,35 @@ function kiwi() {
 }
 
 kiwi();
+
+function addNextButton() {
+  const btn = document.createElement("button");
+  btn.className = "cardbutton";
+  btn.type = "button";
+  btn.id = "nextbutton";
+  const btnText = document.createTextNode("<<");
+  btn.appendChild(btnText);
+
+  document.querySelector(".main").appendChild(btn);
+}
+
+addNextButton();
+document.getElementById("nextbutton").onclick = function() {
+  document.getElementById("newheading").innerHTML = "New bird";
+};
+
+function addBackButton() {
+  const btn = document.createElement("button");
+  btn.className = "cardbutton";
+  btn.type = "button";
+  btn.id = "backbutton";
+  const btnText = document.createTextNode(">>");
+  btn.appendChild(btnText);
+
+  document.querySelector(".main").appendChild(btn);
+}
+
+addBackButton();
+document.getElementById("backbutton").onclick = function() {
+  document.getElementById("newheading").innerHTML = "Kiwi bird";
+};
